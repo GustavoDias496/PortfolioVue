14e7a32a7a7924586fda47e5426eb0b9bf12fa57
GustavoDias496 <gustavodias4966abcd@gmail.com>
2024-05-12 23:09:20 -0300

    corrigindo itens sem alinhamento

--- src/components/CardAbility.vue
+++ src/components/CardAbility.vue
@@ -7,14 +7,8 @@ const props = defineProps(['name', 'srcPath']);
 
 <template>
     <div class="container" ref="containerRef">
-        <span class="name">
-            <a :href="link" target="_blank">{{ name }}</a>
-        </span>
-
+        <span class="name">{{ name }}</span>
         <img :src="srcPath" alt="">
-        <p class="about">
-            {{ about }}
-        </p>
     </div>
 </template>
 
@@ -28,35 +22,24 @@ const props = defineProps(['name', 'srcPath']);
     min-height: 100px;
     border-radius: 3px;
     border: 2px solid var(--color-margin);
-    padding: 15px;
-    background-color: black;
 }
 
 .container img {
     width: 50%;
-    height: 60%;
+    height: auto;
     padding-left: 25%;
     padding-right: 25%;
 }
 
 .container span {
     margin-bottom: 10px;
+    color: var(--color-margin);
+    font-weight: 400;
+    text-align: center;
 }
 
 .container span a {
     text-decoration: none;
-    color: var(--color-text-primary);
-}
-
-.container p {
-    color: var(--color-text-primary);
-    font-size: auto;
-    margin-top: 10px;
-    text-align: justify;
-}
-
-.name {
-    color: var(--color-margin);
-    font-weight: 400;
+    color: var(--color-text-secondary);
 }
 </style>
